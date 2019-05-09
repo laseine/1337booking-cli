dee350f9fc0690c34cb4990a79f6e1586317507e
Mehdi Bounya <ebou-nya@student.1337.ma>
2019-05-09 06:54:37 +0000

    Auth: Add isLogged

--- src/Auth.py
+++ src/Auth.py
@@ -14,7 +14,7 @@ class Auth:
         return response
 
     def logout(self):
-        if keyring.get_password("system", "none") is None:
+        if not self.isLogged():
             return {'error': 'You are not logged in.'}
 
         r = requests.get(self.api_base + "/logout", headers={'Authorization': keyring.get_password("system", "none")})
@@ -25,3 +25,9 @@ class Auth:
         if response['error'] == 'Invalid token.':
             keyring.delete_password("system", "none")
         return response
+
+    def getToken(self):
+        return keyring.get_password("system", "none")
+
+    def isLogged(self):
+        return keyring.get_password("system", "none") is None
